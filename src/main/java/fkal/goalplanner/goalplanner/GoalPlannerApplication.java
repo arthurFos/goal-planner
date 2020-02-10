831078e058773b46abd86b5d6bb16ce72ff59029
Fotso <a_loic16@yahoo.fr>
2020-02-10 15:05:00 +0100

    removed exclude DataSourceAutoConfiguration

--- src/main/java/fkal/goalplanner/goalplanner/GoalPlannerApplication.java
+++ src/main/java/fkal/goalplanner/goalplanner/GoalPlannerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class GoalPlannerApplication {
 
 	public static void main(String[] args) {
